096b6f17f1d141888b8b0cf970d6e40a3c673227
julio Navarro <julionavarro@la-MacBook-Pro-de-julio.local>
2020-03-19 21:00:25 -0600

    Connected cart items with dropdown component by redux

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,15 +1,21 @@
 import { CartActionTypes } from './cart.types';
-
+import { addItemToCart } from './cart.utils'
 const INITAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITAL_STATE, action) => {
     switch(action.type){
         case CartActionTypes.TOGGLE_CART_HIDDEN:
+          return {
+            ...state,
+            hidden: !state.hidden
+          }
+        case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                hidden: !state.hidden
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
